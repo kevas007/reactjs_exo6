ab111fbf2481e4afbd2eb2bfdb954d1839994cd8
kevas007 <kevassiobo@gmail.com>
2021-06-09 09:53:15 +0200

    navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,35 +4,38 @@ import SearchBar from './SearchBar';
 import './css/navigation.css';
 
 class Navigation extends React.Component {
-    render() {
-    return (
-        <nav>
-        <div className="navLeft">
-            <ul>
-                <li>
-                <a>
-                    Link1
-                </a>
-                </li>
-                <li>
-                <a>
-                    Link2
-                </a>
-                </li>
-                <li>
-                    <a>
-                        Link2
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div className="navRight">
-        <SearchBar/>
-        </div>
-    </nav>
-    )
+    changeDirectory = (lien) => {
+        console.log(lien)
     }
-};
+    render() {
+        return (
+            <nav>
+                <div className="navLeft">
+                    <ul>
+                        <li>
+                            <a onClick={() => this.changeDirectory('Accueil')}>
+                                Link 1
+                            </a>
+                        </li>
+                        <li>
+                            <a onMouseOver={() => this.changeDirectory('Galerie')}>
+                                Link 2
+                            </a>
+                        </li>
+                        <li>
+                            <a onDoubleClick={() => this.changeDirectory('Contact')}>
+                                Link 3
+                            </a>
+                        </li>
+                    </ul>
+                </div>
 
+                <div className="navRight">
+                    <SearchBar />
+                </div>
+            </nav>
+        );
+    }
+}
 
 export default Navigation;
